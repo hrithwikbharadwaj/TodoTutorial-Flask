343c8879256a153534ae733dafa167f2a9b5b599
hrithwikbharadwaj <hrithwik009@gmail.com>
2020-09-20 12:12:46 +0530

    made it beautiful

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db = SQLAlchemy(app)
-
+## This is like defining a DB Table with columns like id which is primary key, title and complete.
 class Todo(db.Model):
    id= db.Column(db.Integer, primary_key=True)
    title=db.Column(db.String(100))
    complete= db.Column(db.Boolean)
-
+## This is the index route where we are quering all the rows which are in our DB and passing it to todos
 @app.route('/')
 def index():
    todo_list= Todo.query.all()
    print(todo_list)
    return render_template('base.html',todos=todo_list)
 
+## This is used to add a todo into our DB
 @app.route('/add',methods=["POST"])
 def add():
    name=request.form.get('title')
@@ -48,7 +49,7 @@ def delete(todo_id):
 
 if __name__ == "__main__":
    db.create_all()
-   app.run(debug=True)
+   app.run(debug=True)  
 
 
 
